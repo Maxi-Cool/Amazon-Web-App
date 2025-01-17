3ed9ac7322bcaf7f7f40e25185fc5db9fe565e77
Maxi-Cool <max4mzag@gmail.com>
2025-01-17 15:25:28 -0800

    Add files via upload

---
+++ scrapers.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('button');
+var picture = document.getElementById('pic1');
+
+
+document.getElementById('change').addEventListener("click",function(){
+document.body.innerHTML = "<p>AAAAA</p>";
+});
